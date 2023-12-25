7ce998c91bd0e94ae668c4ee1700bc910bb65bb2
Vladimir Trusevici <vladimir.trusevici.work.email@gmail.com>
2023-12-25 13:13:27 +0200

    P2C-822: refactor returned the return result of the create method of the repository call

--- src/main/java/ru/dobraccoon/painmarket/products/ProductService.java
+++ src/main/java/ru/dobraccoon/painmarket/products/ProductService.java
@@ -4,15 +4,13 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
-    private ProductRepository productRepository;
+    private final ProductRepository productRepository;
 
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
     public Product create(Product newProduct) {
-        Product createdProduct = productRepository.create(newProduct);
-        System.out.println(createdProduct.countPriceWithDiscount());
-        return createdProduct;
+        return productRepository.create(newProduct);
     }
 }
