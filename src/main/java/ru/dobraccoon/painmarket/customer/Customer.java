670edf82355f1b6d56de3b287b022728df3fc266
Vladimir Trusevici <vladimir.trusevici.work.email@gmail.com>
2024-01-10 18:01:18 +0200

    P2C-948: feature Changed the Customer object by requirements

--- src/main/java/ru/dobraccoon/painmarket/customer/Customer.java
+++ src/main/java/ru/dobraccoon/painmarket/customer/Customer.java
@@ -6,6 +6,16 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class Customer {
-    private Long id;
+    private long id;
+    private String imageUrl;
+    private boolean lawEntity;
     private String email;
+    private int PhoneCountryCode;
+    private int phoneNumber;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String city;
+    private String street;
+    private int cityIndex;
 }
