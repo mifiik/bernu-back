e41821ce547a9ad676c4801054e4f6b0c22ca03b
Anatoliy Samoylov <a9295730700@yandex.ru>
2024-01-19 15:18:03 +0300

    P2C-0feature/P2C-000-Configure_pain-market_connection_to_PostgreSQL_database changed folder OrderController added docker-compose.yml made changes to the file application.properties, pom.xml

--- src/main/java/ru/avsamoylov/painmarket/orders/OrderController.java
+++ src/main/java/ru/avsamoylov/painmarket/orders/OrderController.java
@@ -1,12 +1,11 @@
-package ru.avsamoylov.painmarket.customers;
+package ru.avsamoylov.painmarket.orders;
 
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.avsamoylov.painmarket1.product.Order;
-import ru.avsamoylov.painmarket1.service.OrderService;
+
 
 @RestController
 @AllArgsConstructor
--- src/main/java/ru/avsamoylov/painmarket/products/ProductService.java
+++ src/main/java/ru/avsamoylov/painmarket/products/ProductService.java
@@ -2,10 +2,7 @@ package ru.avsamoylov.painmarket.products;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 
 @Service
